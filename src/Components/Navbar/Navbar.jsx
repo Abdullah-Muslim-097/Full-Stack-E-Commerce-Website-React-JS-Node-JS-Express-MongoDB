121fcb840268b268494231def0c99b8859fbf882
Abdullah Muslim <abdullahmuslim383@gmail.com>
2026-03-02 01:43:39 +0500

    Add Navbar component with styling and layout

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Navbar.css'
 
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
 const Navbar = () => {
   return (
     <div className='Navbar'>
+        <div className="Nav-logo">
+            <img src={logo} alt="" />
+            <p>ShopEasy</p>
+        </div>
+        <ul className="Nav-menu">
+            <li>Shop <hr/></li> 
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+        </ul>
+        <div className='Nav-login-cart'>
+            <button>Login</button>
+            <img src={cart_icon} alt="" />
+            <div className="nav-cart-count"></div>
+        </div>
+        
       
     </div>
   )
